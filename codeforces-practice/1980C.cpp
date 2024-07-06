52c5d9bad9c3d02aec806aefd993afc3de3cd641
Abhishek Kumar <abhishek.nitdelhi@gmail.com>
2024-07-06 23:12:50 +0530

    Formatted code.

--- codeforces-practice/1980C.cpp
+++ codeforces-practice/1980C.cpp
@@ -20,36 +20,48 @@ using namespace std;
 #define no cout << "NO" << '\n'
 #define mod 1000000007;
 
-void solve(){
+void solve()
+{
     int n, m;
-    cin>>n;
+    cin >> n;
     vector<int> a(n), b(n);
-    for(int i=0;i<n;i++) cin>>a[i];
-    for(int i=0;i<n;i++) cin>>b[i];
-    cin>>m;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+    cin >> m;
     vector<int> d(m);
-    for(int i=0;i<m;i++) cin>>d[i]; // the change array having the operations
+    for (int i = 0; i < m; i++)
+        cin >> d[i]; // the change array having the operations
     map<int, int> freq;
-    for(int num:d) freq[num]++;
+    for (int num : d)
+        freq[num]++;
     int cnt = 0;
-    for(int i=0;i<n;i++){
-        if(a[i] == b[i]) continue;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == b[i])
+            continue;
         cnt++;
-        if(freq.find(b[i]) == freq.end() or freq[b[i]]<=0){
+        if (freq.find(b[i]) == freq.end() or freq[b[i]] <= 0)
+        {
             no;
             return;
         }
         freq[b[i]]--;
-        if(cnt > m){
+        if (cnt > m)
+        {
             no;
             return;
         }
     }
     set<int> st;
-    for(int num:b) st.insert(num);
+    for (int num : b)
+        st.insert(num);
 
-    for(int num:d){
-        if(st.find(num) == st.end()){
+    for (int num : d)
+    {
+        if (st.find(num) == st.end())
+        {
             no;
             return;
         }
@@ -61,7 +73,8 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
-    cin>>t;
-    while(t--) solve();
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
